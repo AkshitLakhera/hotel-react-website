d83e131980b7ee76d78122cedb4ba32899dd22ee
“[Akshit <akshitlakhera14@gmail.com>
2024-05-09 16:59:55 +0530

    Updated

--- Rajveer hotel/src/components/Threedcard.jsx
+++ Rajveer hotel/src/components/Threedcard.jsx
@@ -1,8 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import { CardBody, CardContainer, CardItem } from "../components/ui/3d-card";
 
 export const ThreeDCard = ({ title, imageUrl, price, description }) => {
+  const navigate = useNavigate("");
   return (
-    <CardContainer className="inter-var">
+    <CardContainer className="inter-var" onClick={() => navigate("contact")}>
       <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
         <CardItem
           translateZ="50"
@@ -26,13 +28,16 @@ export const ThreeDCard = ({ title, imageUrl, price, description }) => {
         </CardItem>
         <div className="flex justify-between items-center mt-10">
           <a
-            href="https://twitter.com/mannupaaji"
+            href=""
             target="_blank"
             className="px-4 py-2 rounded-xl text-3xl font-bold text-slate-600 dark:text-white font-sans"
           >
             {price}
           </a>
-          <button className="px-4 py-3 rounded-xl bg-yellow-500  text-white text-xs font-bold font-sans">
+          <button
+            className="px-4 py-3 rounded-xl bg-yellow-500  text-white text-xs font-bold font-sans"
+            onClick={() => navigate("/contact")}
+          >
             Book Now
           </button>
         </div>
