54927703a99e39f242636924b9ca176cdc05e851
“[Akshit <akshitlakhera14@gmail.com>
2024-05-09 15:29:02 +0530

    Floating whatsapp added

--- Rajveer hotel/src/pages/Home.jsx
+++ Rajveer hotel/src/pages/Home.jsx
@@ -5,9 +5,18 @@ import { Roomcomp } from "../components/Roomcomp";
 import { Gallery } from "../components/Gallery";
 import { Footerbox } from "../components/Footer";
 import { Amenities } from "../components/Amenties";
+import { FloatingWhatsApp } from "react-floating-whatsapp";
+import image18 from "../assets/image18.jpg";
 export default function Home() {
   return (
     <div className="bg-slate-50">
+      <div className="relative top-9 left-9">
+        <FloatingWhatsApp
+          phoneNumber={"9758584612"}
+          accountName={"Rajveer HomeStay"}
+          avatar={image18}
+        />
+      </div>
       <Navbar />
       <Navslideshow />
       <Overview />
